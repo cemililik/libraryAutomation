51793ccbc3ac7432e558fb37ea02e8aa4ab2b44a
Cemil ILIK <cemililik@outlook.com>
2017-12-22 20:51:29 +0300

    Project file opened Nuget packages updated

---
+++ Library_Automation/Library_Automation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Library_Automation.Startup))]
+namespace Library_Automation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
